4d2593d33e09b3acdf88c3265680f210a62322a6
DESKTOP-N59VG71\Aracely <raul.guerrero2703@gmail.com>
2018-04-09 00:11:20 -0500

    Ejemplo de conexión JAVA  con SQL Server para Android Apps

---
+++ app/src/main/java/com/linkcode/sql/mr2guer/ejemploconexionsql/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.linkcode.sql.mr2guer.ejemploconexionsql;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        String valor = "Bienvenido: " + getIntent().getStringExtra("usuario");
+
+        texto = (TextView)findViewById(R.id.textView);
+
+        texto.setText(valor);
+
+    }
+
+}
---
+++ app/src/main/java/com/linkcode/sql/mr2guer/ejemploconexionsql/MainActivity.java
@@ -0,0 +1,124 @@
+package com.linkcode.sql.mr2guer.ejemploconexionsql;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConnectionClass connectionClass;
+    EditText edtuserid,edtpass;
+    Button btnlogin;
+    ProgressBar pbbar;
+    public String Valor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        connectionClass = new ConnectionClass(); //&nbsp;//the class file
+        edtuserid = (EditText) findViewById(R.id.edtuserid);
+        edtpass = (EditText) findViewById(R.id.edtpass);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        pbbar = (ProgressBar) findViewById(R.id.pbbar);
+        pbbar.setVisibility(View.GONE);
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DoLogin doLogin = new DoLogin(); //&nbsp;// this is the Asynctask
+                doLogin.execute("");
+            }
+        });
+
+
+    }
+    public class DoLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+
+
+        String userid = edtuserid.getText().toString();
+        String password = edtpass.getText().toString();
+
+
+        @Override
+        protected void onPreExecute() {
+            pbbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r) {
+            pbbar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this,r,Toast.LENGTH_SHORT).show();
+
+            if(isSuccess) {
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("usuario", userid.toString());
+                startActivity(i);
+                finish();
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(userid.trim().equals("")|| password.trim().equals(""))
+                z = "Please enter User Id and Password";
+            else
+            {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        z = "Error in connection with SQL server";
+                    } else {
+                        String query = "select TIPO_FALLA FROM DT_FALLOS_MANTENIMIENTO WHERE ID =2";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        if(rs.next())
+                        {
+                            //Valor = rs.getString(0);
+                            z = "Login successfull";
+                            isSuccess=true;
+                        }
+                        else
+                        {
+                            z = "Invalid Credentials";
+                            isSuccess = false;
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = "Exceptions";
+                }
+            }
+            return z;
+        }
+    }
+
+}
+
